95d920d9ee57155560a2fbe4064b99ace5d5af33
adikrisnaffa <adikrisnanugraha@gmail.com>
2023-11-28 09:53:04 +0700

    add script powershell

---
+++ Change Format Document File SQL .ps1
@@ -0,0 +1,17 @@
+﻿# Function to create SQL Server script with SET ANSI_NULLS ON and SET QUOTED_IDENTIFIER ON
+function Create-SqlScript {
+    param (
+        [string]$FilePath
+    )
+
+    $sqlScript = Get-Content $FilePath -Raw
+
+    # Add GO before each ALTER PROCEDURE statement
+    $sqlScript = $sqlScript -replace '(ALTER PROCEDURE[^\r\n]+)\r?\n', "GO`r`n`$1`r`n"
+
+    # Save the modified script
+    $sqlScript | Out-File -FilePath $FilePath -Encoding UTF8
+}
+
+# Example: Provide the path to your SQL script
+Create-SqlScript -FilePath "....."
---
+++ Collect DB Stored Procedures on Local Machine.ps1
@@ -0,0 +1,54 @@
+﻿Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+# Install the SqlServer module
+Install-Module -Name SqlServer -Force -AllowClobber -Scope CurrentUser
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.ConnectionInfo") | Out-Null
+
+# Memuat Assembly SMO
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | Out-Null
+
+# Menentukan informasi koneksi
+$serverInstance = "....."
+$databaseName = "....."
+$username = "....."
+$password = "....."
+
+# Nama file output
+$outputFile = "....."
+
+# Menulis pernyataan USE
+$useStatement = "USE [$databaseName]`nGO`n"
+$useStatement | Out-File -Append $outputFile
+
+# Membuat objek ServerConnection
+$serverConnection = New-Object Microsoft.SqlServer.Management.Common.ServerConnection($serverInstance, $username, $password)
+
+# Membuat Objek Server dengan menggunakan ServerConnection
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($serverConnection)
+
+# Mendapatkan Objek Database
+$database = $server.Databases[$databaseName]
+
+# Menggunakan Metode Script() untuk Stored Procedures
+$storedProceduresScript = $database.StoredProcedures | Where-Object {
+    # Filter stored procedures berdasarkan kriteria
+    -not $_.IsSystemObject -and
+    $_.Name -notlike 'sys.sp_FuzzyLookupTableMaintenance%'
+} | ForEach-Object {
+    try {
+        # Mengganti CREATE dengan ALTER
+        $script = $_.Script().Replace("CREATE PROCEDURE", "ALTER PROCEDURE")
+        # Menambahkan pernyataan GO setelah setiap stored procedure
+        $script += "`nGO`n"
+        $script
+    }
+    catch {
+        Write-Host "Failed to script stored procedure $($_.Name): $_"
+    }
+}
+
+# Menyimpan Hasil ke File
+$storedProceduresScript | Out-File -Append $outputFile
+
+Write-Host "Stored Procedures script has been generated to: $outputFile"
